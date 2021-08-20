be44bac4e21f2a8b6ac98557dc3d58d20afadabd
Durgaprasath01415 <sdp8754262827@gmail.com>
2021-08-20 10:36:17 +0530

    Test 3 commit to check jenkins

--- src/main/java/com/bookstore/main/BookStoreAppApplication.java
+++ src/main/java/com/bookstore/main/BookStoreAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
+
 public class BookStoreAppApplication {
 
 	public static void main(String[] args) {
