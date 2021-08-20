f8d5813af5b42052faa5fb37bf1845f5f516904f
Durgaprasath01415 <sdp8754262827@gmail.com>
2021-08-20 09:54:00 +0530

    Test2 commit to check jenkins

--- src/main/java/com/bookstore/main/BookStoreAppApplication.java
+++ src/main/java/com/bookstore/main/BookStoreAppApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.bookstore.main")
 @EnableEurekaClient
 public class BookStoreAppApplication {
 
